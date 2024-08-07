f209c33b7f1a4e09823d5c1358bcc8c8f953966d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-07 19:50:41 -0300

    feat: import useForm

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import * as S from "./style"
 import { FaPlusCircle } from "react-icons/fa";
+import { useForm } from "useform-simple-hook";
 
 export const Form: React.FC = () => {
+    const [register, handleSubmit] = useForm
+
     return (
         <>
             <S.ContainerSection>
-                <S.FormTask>
+                <S.FormTask >
                     <S.InputTask type="text" placeholder="Add Task" />
                     <S.ButtonAddTask><FaPlusCircle size={"32px"} /></S.ButtonAddTask>
                 </S.FormTask>
