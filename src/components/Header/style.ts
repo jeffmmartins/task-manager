1deec8035c3421faeb179fd5bb4a0012ae7f0f21
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-17 12:59:28 -0300

    style: Header component updated

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-margin: 30px;
+padding: 30px;
 text-align: center;
 max-width: 1920px;
+background-color: #0001;
+
+border-radius: 0px 0px 100px 100px;
 `
 export const Title = styled.h1`
 color: #08f;
