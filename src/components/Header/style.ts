e6eafaa9c629e577296c0a25e0563c2c3b2b3a92
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-17 12:39:06 -0300

    style: change the color of text

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-background-color: #ff6f61;
+margin: 30px;
 text-align: center;
 max-width: 1920px;
 `
 export const Title = styled.h1`
+color: #08f;
 font-weight: 700;
 `
