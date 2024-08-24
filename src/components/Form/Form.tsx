3417545b3bd7703c270090708ec9d804c7796453
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-24 10:38:20 -0300

    feat: button remove is done

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useId } from "react";
+import uuid from "react-uuid";
 import * as S from "./style"
 import { FaPlusCircle } from "react-icons/fa";
 import {MdOutlineCheckBoxOutlineBlank } from "react-icons/md"
@@ -8,16 +8,17 @@ import { Footer } from "../Footer/Footer";
 
 interface DataInput {
     name: string;
-    id: string;
+
 }
 
 interface IFormInput {
     name: string;
-    id:string;
 }
 
+
+
+
 export const Form: React.FC = () => {
-    const taskId = useId()
     const tasks = localStorage.getItem('tasks')
     const info: string[] = JSON.parse(tasks || '[]')
     const [task, setTask] = useState<string[]>(info);
@@ -30,7 +31,6 @@ export const Form: React.FC = () => {
             alert("Favor Preencher campo")
             return
         }
-        console.log(taskId)
         setTask([...task, formData.name]);
         reset();
     }
@@ -39,26 +39,27 @@ export const Form: React.FC = () => {
         localStorage.setItem("tasks", JSON.stringify(task))
     }, [task])
 
-    /*
-    const remove = () => {
-        const newTask = task.filter((tasks)=> )
+    
+    const remove = (index: number) => {
+        const newTask = task.filter((_,i)=> i !== index)
+        setTask(newTask)
     }
-    */
+    
 
     return (
         <>
             <S.ContainerSection>
                 <S.FormTask onSubmit={handleSubmit(data)}>
-                    <S.InputTask type="text" placeholder="Add Task"  {...register("name", {required: taskId})} />
+                    <S.InputTask type="text" placeholder="Add Task"  {...register("name")} />
                     <S.ButtonAddTask><FaPlusCircle size={"32px"} /></S.ButtonAddTask>
                 </S.FormTask>
             </S.ContainerSection>
             <S.TaskSection>
                 <S.ContainerTask>
-                    {task.map((tasks, index) =>
+                    {task.map((tasks,index) =>
                     (<S.ListTasks>
                         <S.ItemTask key={index}>{tasks} 
-                            <S.ButtonCheck><MdOutlineCheckBoxOutlineBlank size={"25px"}/></S.ButtonCheck>
+                            <S.ButtonCheck onClick={() => (remove(index))}><MdOutlineCheckBoxOutlineBlank size={"25px"}/></S.ButtonCheck>
                         </S.ItemTask>
                     </S.ListTasks>))}
                 </S.ContainerTask>
