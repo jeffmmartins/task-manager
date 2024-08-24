719285945cb3ef11b7d145ff6d085e24b2a707e0
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-24 09:55:20 -0300

    install uuid

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useId } from "react";
 import * as S from "./style"
 import { FaPlusCircle } from "react-icons/fa";
-import { useForm } from "react-hook-form";
 import {MdOutlineCheckBoxOutlineBlank } from "react-icons/md"
 import { Footer } from "../Footer/Footer";
 
 interface DataInput {
     name: string;
+    id: string;
 }
 
 interface IFormInput {
     name: string;
+    id:string;
 }
 
 export const Form: React.FC = () => {
+    const taskId = useId()
     const tasks = localStorage.getItem('tasks')
     const info: string[] = JSON.parse(tasks || '[]')
     const [task, setTask] = useState<string[]>(info);
@@ -22,10 +26,11 @@ export const Form: React.FC = () => {
 
 
     const data = (formData: DataInput) => {
-        if (!formData.name) {
+        if (!formData) {
             alert("Favor Preencher campo")
             return
         }
+        console.log(taskId)
         setTask([...task, formData.name]);
         reset();
     }
@@ -34,16 +39,17 @@ export const Form: React.FC = () => {
         localStorage.setItem("tasks", JSON.stringify(task))
     }, [task])
 
+    /*
     const remove = () => {
-        const newTask = task.filter(()=>)
+        const newTask = task.filter((tasks)=> )
     }
-    
+    */
 
     return (
         <>
             <S.ContainerSection>
                 <S.FormTask onSubmit={handleSubmit(data)}>
-                    <S.InputTask type="text" placeholder="Add Task" {...register("name")} />
+                    <S.InputTask type="text" placeholder="Add Task"  {...register("name", {required: taskId})} />
                     <S.ButtonAddTask><FaPlusCircle size={"32px"} /></S.ButtonAddTask>
                 </S.FormTask>
             </S.ContainerSection>
@@ -52,7 +58,7 @@ export const Form: React.FC = () => {
                     {task.map((tasks, index) =>
                     (<S.ListTasks>
                         <S.ItemTask key={index}>{tasks} 
-                            <S.ButtonCheck onClick={remove}><MdOutlineCheckBoxOutlineBlank size={"25px"}/></S.ButtonCheck>
+                            <S.ButtonCheck><MdOutlineCheckBoxOutlineBlank size={"25px"}/></S.ButtonCheck>
                         </S.ItemTask>
                     </S.ListTasks>))}
                 </S.ContainerTask>
